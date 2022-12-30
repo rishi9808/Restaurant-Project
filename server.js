ab64cffdfbfd434e557df0695313e19629c78a01
Rishikesh K V <rishikeshkaroth@gmail.com>
2022-12-30 20:52:22 +0530

    Initial setup

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const ejs = require('ejs')
+const path = require('path')
+const expressLayout = require('express-ejs-layouts')
+const app =express()
+//Port setting
+const PORT = process.env.PORT || 3300
+
+app.get('/',(req,res)=>{
+    res.render('home')
+})
+
+//set Template engine
+app.use(expressLayout)
+app.set('views',path.join(__dirname,'/resources/views'))
+app.set('view engine','ejs')
+
+
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`Listening on port ${PORT}`)
+})
